618a4e264cc03e81fc0f2ec6a3533bfb4663be77
brianmatus <35672931+brianmatus@users.noreply.github.com>
2020-11-09 00:10:32 -0600

    .

--- assets/js/individual_recipe.js
+++ assets/js/individual_recipe.js
@@ -14,17 +14,9 @@ $( document ).ready(function() {
   updateLoggedUserAndFillRecipeInfo()
 });
 
-
-
-
-
-
-
 function updateLoggedUserAndFillRecipeInfo() {
   console.log("Empezando inicio de sesion");
   var request = async() => {
-
-
 
     var requestOptions = {
       method: 'GET',
@@ -36,7 +28,7 @@ function updateLoggedUserAndFillRecipeInfo() {
     var userData = await userFetch.json()
 
     console.log(userData);
-    console.log("Inicio de sesion con exito");
+    console.log("Inicio de sesion procesado con exito");
     if (userData.RETURNCODE == 0) {
       loggedUser = userData.RESULT
       console.log("Server-side userloggon detected, updated now to:");
@@ -46,8 +38,8 @@ function updateLoggedUserAndFillRecipeInfo() {
 
     if(userData.RETURNCODE == 1) {
       //TODO No puede reaccionar ni comentar
+      $("#commentButton").prop('disabled', true);
     }
-
 
     var uid = urlParams.get('uid');
     var myHeaders = new Headers();
@@ -74,9 +66,106 @@ function updateLoggedUserAndFillRecipeInfo() {
     $("#time").html("Tiempo:<br>" + recipeInfo.time)
 
 
-
-
-
+    //*Comentarios
+    for (var comment in recipeInfo.comments) { if (!recipeInfo.comments.hasOwnProperty(comment)) {continue}
+      console.log("Sending to add html");
+      console.log(recipeInfo.comments[comment]);
+      addCommentToHTML(recipeInfo.comments[comment])
+    }
   }
   request()
+}
+
+function addCommentToHTML(comment) {
+
+  var template = $("#template-comment").html();
+  var html = Mustache.render(template, comment);
+  $("#commentsContainer").append(html)
+
+}
+
+
+
+
+
+
+
+$("#navigationLoginButton").click( function(e) {
+  e.preventDefault();
+  if (loggedUser == null) {
+    $("#modalLRForm").modal("show");
+  }
+  else {
+    fetch(BACK_URL + "logout/", {
+      method: "POST",
+      body: ""
+    }).then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+    loggedUser = null
+    $("#navigationLoginButton").text("Iniciar Sesion")
+  }
+} );
+
+
+$("#loginButton").click( function(e) {
+  e.preventDefault();
+  var username = $("#modalLRInput10").val();
+  var password = $("#modalLRInput11").val();
+
+  let data = {username: username, password: password}
+  fetch(BACK_URL + "login/", {
+    method: "POST",
+    body: JSON.stringify(data)
+  }).then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    loginReturn(data, username, password)
+    loggedUser = data.RESULT
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+});
+
+function loginReturn(data, enteredUsername, enteredPassword) {
+
+  console.log(data);
+  console.log(enteredUsername);
+  console.log(enteredPassword);
+
+  switch(data.RETURNCODE) {
+    case "-1":
+      swal("Informacion incompleta", "Debes ingresar todos los datos requeridos", "warning");
+      break;
+    case "0":
+      swal("Inicio de sesion exitoso", "Bienvenido," + enteredUsername, "success");
+      $("#modalLRForm").modal("hide");
+      $("#navigationLoginButton").text("Cerrar Sesion")
+      $("#commentButton").prop('disabled', false);
+      break;
+    case "1":
+      swal("Usuario no encontrado", "No se encontro ningun usuario con username " + enteredUsername, "warning");
+      break;
+    case "2":
+    swal("Contraseña invalida", "La Contraseña ingresada (" + enteredPassword + ") no coincide con la del usuario", "error");
+      break;
+    case "3":
+      swal("Good job!", "You clicked the button!", "success");
+      break;
+    case "4":
+      swal("Good job!", "You clicked the button!", "success");
+      break;
+    default:
+  }
+
+  console.log(data.USERTYPE);
+  if (data.USERTYPE == "admin") {
+    var audio = new Audio('assets/mp3/soy_admin.mp3');
+    audio.play();
+  }
 }
